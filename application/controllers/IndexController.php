546b74e38286e95d349c0ee6ceff902be3e72ed3
Solkor <dp.andrey@mail.ru>
2016-05-04 12:55:56 +0300

    link

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -151,30 +151,30 @@ class IndexController extends Zend_Controller_Action
                 <script type="text/javascript" src="http://bt.3tv.im/dash/js/ParsedQueryString.js"></script>
                 <script src="http://bt.3tv.im/dash/js/jquery_1.8.3.js"></script>
                 <script src="http://bt.3tv.im/dash/js/jquery.contextmenu.js"></script>
-    <div id="VIDEOPLAYER_WRAP">
-        <video id="video" class="video-js vjs-default-skin" controls autoplay preload="auto" width="100%" height="100%"></video>
-    </div>
-
-    <script>
-    var FileListId = "'.$FileListId.'";
-    </script>
-  <script src="http://bt.3tv.im/dash/js/video.js"></script>
-  <script src="http://bt.3tv.im/dash/js/hls.js"></script>
-  <script src="http://bt.3tv.im/dash/js/videojs-hlsjs.js"></script>
-  <script src="http://bt.3tv.im/dash/js/videojs-resolution-switcher.js"></script>
-  <script src="http://bt.3tv.im/dash/js/videojs-audio-switcher.js"></script>
-  <script src="http://bt.3tv.im/dash/js/vast-client.js"></script>
-  <script src="http://bt.3tv.im/dash/js/vast/video.ads.js"></script>
-  <script src="http://bt.3tv.im/dash/js/vast/videojs.vast.js"></script>
-  <script src="http://bt.3tv.im/dash/js/videojs-playlists-old.js"></script>
-  <script src="http://bt.3tv.im/dash/js/videojs.hotkeys.js"></script>
-  <script src="http://bt.3tv.im/dash/js/canvas.js"></script>
-  <script src="http://bt.3tv.im/dash/js/metrics.js"></script>
-  <script src="http://bt.3tv.im/dash/js/jsonpack.js"></script>
-  <script src="http://bt.3tv.im/dash/js/js.cookie.js"></script>
-  <script src="http://bt.3tv.im/dash/js/app_old.js"></script>
-  <script src="http://bt.3tv.im/dash/js/app-plugins.js"></script>
-  <script src="http://bt.3tv.im/dash/js/app.js"></script>
+                <div id="VIDEOPLAYER_WRAP">
+                    <video id="video" class="video-js vjs-default-skin" controls autoplay preload="auto" width="100%" height="100%"></video>
+                </div>
+            
+                <script>
+                var FileListId = "'.$FileListId.'";
+                </script>
+              <script src="http://bt.3tv.im/dash/js/video.js"></script>
+              <script src="http://bt.3tv.im/dash/js/hls.js"></script>
+              <script src="http://bt.3tv.im/dash/js/videojs-hlsjs.js"></script>
+              <script src="http://bt.3tv.im/dash/js/videojs-resolution-switcher.js"></script>
+              <script src="http://bt.3tv.im/dash/js/videojs-audio-switcher.js"></script>
+              <script src="http://bt.3tv.im/dash/js/vast-client.js"></script>
+              <script src="http://bt.3tv.im/dash/js/vast/video.ads.js"></script>
+              <script src="http://bt.3tv.im/dash/js/vast/videojs.vast.js"></script>
+              <script src="http://bt.3tv.im/dash/js/videojs-playlists-old.js"></script>
+              <script src="http://bt.3tv.im/dash/js/videojs.hotkeys.js"></script>
+              <script src="http://bt.3tv.im/dash/js/canvas.js"></script>
+              <script src="http://bt.3tv.im/dash/js/metrics.js"></script>
+              <script src="http://bt.3tv.im/dash/js/jsonpack.js"></script>
+              <script src="http://bt.3tv.im/dash/js/js.cookie.js"></script>
+              <script src="http://bt.3tv.im/dash/js/app_old.js"></script>
+              <script src="http://bt.3tv.im/dash/js/app-plugins.js"></script>
+              <script src="http://bt.3tv.im/dash/js/app.js"></script>
 
 ';
         header("Access-Control-Allow-Origin: http://bt.3tv.im ");
