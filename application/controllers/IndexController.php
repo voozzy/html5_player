ec7a7a7c7e148d505dd1e00648cc8f6313b64584
Solkor <dp.andrey@mail.ru>
2016-05-11 12:24:16 +0300

    voozzy

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -7,11 +7,13 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
-        $url = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
-        if($url !== 'm.tree.tv'){
-            $this->_helper->layout->setLayout('voozzy_layout');
+        if(!is_null($_SERVER['HTTP_REFERER'])){
+            $url = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+
+            if($url !== 'm.tree.tv'){
+                $this->_helper->layout->setLayout('voozzy_layout');
+            }
         }
-        
         
         $this->_db = Zend_Db_Table::getDefaultAdapter();
     }
